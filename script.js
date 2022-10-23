fb9b747f271e503f071a3416f2829c952fbbb764
Meece <meece.work@gmail.com>
2022-10-23 18:31:52 +0200

    Added review styling

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const nav = document.getElementById('nav');
 const navBtn = document.querySelector('.nav-bars-btn');
 const closeBtns = document.querySelectorAll('#nav .close-btn');
+const reviews = document.querySelectorAll('.review');
+const expandBtn = document.querySelectorAll('.review .expand');
+const collapseBtn = document.querySelectorAll('.review .collapse');
 
 closeBtns.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -12,4 +15,16 @@ closeBtns.forEach(btn => {
 navBtn.addEventListener('click', () => {
     nav.classList.add('show');
     nav.classList.remove('collapse');
+});
+
+expandBtn.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        reviews[idx].classList.add('show');
+    });
+});
+
+collapseBtn.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        reviews[idx].classList.remove('show');
+    });
 });
